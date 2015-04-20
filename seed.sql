ef0c46d1e8fea5df16259d5100698b1d9b4af174
JAH <kwasijames@OtuJAH.local>
2015-04-20 10:43:07 -0400

    Created head, footer and README

--- seed.sql
+++ seed.sql
@@ -1,6 +1,60 @@
-INSERT INTO micro_posts(title, body, author_id, snippet_id, tag_id) VALUES ("KNICKS WIN", "Score 103-98 blanddeieioewo. fjdsjkdskdskds. djkdskjdsjkds", "1", "1", "1");
+-- dummy data
 
-INSERT  INTO authors(name, email) VALUES ("John Smith", "Johnsmith@gmail.com");
+INSERT INTO micro_posts(title, body, author_id, snippet_id, tag_id) VALUES ("A top-two pick? Knicks have a chance", "The New York Knicks are guaranteed a top-five pick in the NBA draft.
+
+We won’t know where they'll pick until May 19, when the league holds its draft lottery. But we have an idea of the odds of specific scenarios.
+
+Below, we offer a breakdown of the odds for the Knicks, per ESPN's Ben Alamar and the analytics group:
+
+No. 1 overall: 19.9 percent
+
+No. 2 overall: 18.8 percent
+
+Top 2: 38.7 percent
+
+No. 3 overall: 17.1 percent
+
+Top 3: 55.8 percent
+
+No. 4 overall: 31.9 percent
+
+Top 4: 87.7 percent
+
+No. 5: 12.3 percent
+
+Top 5: 100 percent
+
+New York (17-65) finished with the second-worst record in the NBA, just ahead of the Minnesota Timberwolves, so Minnesota has the best odds to get the top pick (25 percent).
+
+The Knicks are using the top pick as one of the main tools in their rebuild this summer. They also have $25 million in cap space. President Phil Jackson and GM Steve Mills have said that they will listen to offers from teams who want to trade for the pick. Their preference, though, is to hold on to it.
+
+Cleanthony confident in future: Jackson and coach Derek Fisher addressed the Knicks shortly after their season-ending loss to Detroit last Wednesday.
+
+According to one player in the room, the message was simple.
+
+Jackson and Fisher thanked the players for their efforts in a tough year and assured them that the team would be better next season.
+
+Of course, not every player in the locker room last Wednesday will be around for the potential turnaround.
+
+Jackson hopes to re-sign a handful of players from this year’s team. Langston Galloway, Lou Amundson and Lance Thomas are strong candidates to be on the roster in 2015-16. But most of the roster from the worst season in franchise history will not be back.
+
+One player who will certainly be back is rookie Cleanthony Early.
+
+Early, Jackson’s first draft pick, admitted earlier this month that he didn’t have the type of season he was hoping to.
+
+But he remains confident that he can be a part of the Knicks' future.
+
+So what does he need to work on?
+
+“Just loosening up a little bit,” Early said after the final game of the season. “I’m a basketball player. I have the talent, but sometimes you think too much. And you’ve just got to go out there and play. The time, the experience of being on court with those caliber players [this season] just helped.
+
+“I’ve just got to continue to work hard. The talent’s there. I just got to continue to work hard and find ways to show it and connect the dots -- that’s what we’re all doing.”
+
+Early suffered multiple ankle injuries this season and said his ankle never felt 100 percent. He also had knee surgery early in the season.
+
+The former Wichita State star played in 39 games, scoring 5.4 points per game and shooting 35 percent from the field. That’s not the kind of production the Knicks were looking for when they drafted Early, but it’s way too early to call the second-round pick a bust.", "1", "1", "1");
+
+INSERT  INTO authors(name, email) VALUES ("Ian Begley", "ibegley@espn.com");
 
 INSERT INTO snippets(body) VALUES ("SJSJKDSKDSKDSJKDSJDS.");
 
--- server.js
+++ server.js
@@ -44,22 +44,25 @@ app.get("/posts", function(req, res){
 				var authors = data;
 				//console.log(authors);
 			}
-		// get a snippet of the micro_post
+
+		// Snippets // get the body of the micropost and take a snippet and place it in snippets table and link with the micro_posts table with the snippet_id	
+		/* get a snippet of the micro_post
 		db.all("SELECT * FROM snippets INNER JOIN micro_posts ON micro_posts.snippet_id = snippet_id", function(err, data){
 			if(err){
 				console.log(err);
 			}
 			else{
 				var snippets = data;
-				//console.log(snippets);
+				console.log(snippets);
 			}
 
-		res.render("index.ejs", {posts: posts, authors: authors, snippets: snippets});
-			});	
+			});	*/
+		res.render("index.ejs", {posts: posts, authors: authors});
 		
 		});
 	});
 });
+
 
 // serve up a form to create a new micro_post
 app.get("/posts/new", function(req, res){
@@ -69,7 +72,7 @@ app.get("/posts/new", function(req, res){
 
 // Create new post insert into database and render new post on "/posts"
 app.post("/posts", function(req, res){
-	console.log(req.body.title, " ", req.body.body, "", req.body.author); 
+	//console.log(req.body.title, " ", req.body.body, "", req.body.author); 
 
 			//Get info from req.body to update Knicks_feed.db
 	db.run("INSERT INTO authors(name) VALUES (?)", req.body.author, function(err){
@@ -77,20 +80,29 @@ app.post("/posts", function(req, res){
 			console.log(err);
 		}
 
+
+
 	  db.get("SELECT id FROM AUTHORS WHERE name =?", req.body.author, function(err, data){
    		if(err){
    			console.log(err);
    		}
    		else{
    			var author_id = data.id;
-   			console.log(author_id);
+   			//console.log(author_id);
    		}
 
    			   	db.run("INSERT INTO micro_posts(title, body, author_id) VALUES (?, ?, ?)", req.body.title, req.body.body, author_id, function(err){
    			   		if(err){
    			   			console.log(err);
    			   		}
-   			   		console.log(author_id);
+   			   		console.log(req.body.body);
+
+   			   		// CREATE SNIPPETS
+   			   		var body = req.body.body.split('.');   // to hold the contents of the body
+   			   		var snippet = body[0] +  ". " + body[1];
+					console.log(snippet);
+
+					db.run("INSERT INTO snippets(body) VALUES (?)", snippet);		   		 
 
    	res.redirect("/posts");
    });
@@ -99,20 +111,37 @@ app.post("/posts", function(req, res){
 });
 });
 
+
+
 // Show individual micro posts
-app.get("/posts/:id", function(req, res){
+app.get("/posts/:id/", function(req, res){
+	/* get the title and bod from micro post table
 	db.get("SELECT * FROM micro_posts WHERE id = ?", req.params.id, function(err, data){
 		if(err){
 			console.log(err);
 		}
 		else{
 			var thisPost = data;
-		    console.log(thisPost);
-		    res.render("show.ejs", {thisPost: thisPost});
-		}
+		    //console.log(thisPost);    
+		}*/
+
+		console.log(req.params.id);
+		console.log(req);
+
+		// get the author's name of the article from authors table using author_id, and article from the micro_posts table
+		db.get("SELECT * FROM micro_posts INNER JOIN authors ON micro_posts.author_id = ?", req.params.id, function(err, data){
+			if(err){
+				consol.log(err);
+			}
+			else{
+				var thisPost = data;
+				//console.log(thisPost);
+			}
+			res.render("show.ejs", {thisPost: thisPost});
+		});
+		
 	});
 
-});
 
 
 
